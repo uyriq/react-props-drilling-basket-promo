904ea16188c73e51bde1e47a79344ca0d6fe72a3
uyriq <105006434+uyriq@users.noreply.github.com>
2022-07-09 20:38:33 +0300

    clean

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/app/app";
 
-const styles = {
-  card: { padding: 50, maxWidth: 800, margin: "0 auto 300px" },
-  button: { display: "flex", marginLeft: "auto" }
-};
-
 const rootElement = document.getElementById("react-root");
 const root = createRoot(rootElement);
 root.render(
